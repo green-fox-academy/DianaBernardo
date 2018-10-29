62e9b5b3db31930e601cd5265b407107b76103e2
Diana <dianafbernardo@gmail.com>
2018-10-29 13:10:59 +0100

    Week 2 added

---
+++ Week-02/20181029/src/HelloOthers.java
@@ -0,0 +1,13 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello, Chika");
+        System.out.println("Hello, Bettina");
+        System.out.println("Hello, Dori");
+    }
+}
---
+++ Week-02/20181029/src/IntroduceYourself.java
@@ -0,0 +1,24 @@
+public class IntroduceYourself {
+
+
+    public static void main(String[] args) {
+        // Write a program that prints a few details to the terminal window about you
+        // It should print each detail to a new line.
+        //  - Your name
+        //  - Your age
+        //  - Your height in meters (as a decimal fraction)
+        //
+        //  Example output:
+        //  John Doe
+        //  31
+        //  1.87
+
+       String name = "Diana Bernardo";
+       int age = 30;
+       double height = 1.63;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
