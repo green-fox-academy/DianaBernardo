c69214c4355268029a6d239fcb828f89c02267cd
Diana <dianafbernardo@gmail.com>
2018-10-30 09:08:37 +0100

    Almost all exercices of day 1

---
+++ Week-02/20181029/src/DrawChessTable.java
@@ -0,0 +1,25 @@
+public class DrawChessTable {
+    public static void main(String[] args) {
+        // Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+
+        for (int i = 1; i <= 8; i++) {
+
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("% ");
+            }
+            System.out.println(" ");
+        }
+
+                System.out.println(" ");
+            }
+        }
