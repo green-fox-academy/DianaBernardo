d5d4494c369c52b7b667d252254bba7985c96f03
Diana <dianafbernardo@gmail.com>
2018-10-31 11:40:24 +0100

    Complete until sum all

--- Week-02/Day-02/src/AppendA2.java
+++ Week-02/Day-02/src/AppendA2.java
@@ -3,14 +3,22 @@
 // - Add all elements an `"a"` at the end
 
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class AppendA2 {
     public static void main(String[] args) {
         String[] animals = {"koal", "pand", "zebr"};
+
+        String x = Arrays.toString(animals);
+        for(int i=0; i<animals.length; i++){
+            System.out.println(appendAFunc(animals[i]));
+        }
     }
 
+
+    public static String appendAFunc(String args) {
+        StringBuilder sb = new StringBuilder("a");
+        return args + sb;
+    }
 }
-
-        public static String appendAFunc(String args) {
-            StringBuilder sb = new StringBuilder("a");
-            return args + sb;
-    }
