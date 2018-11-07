fc2fb4c8dcb629f9ef42489688860ab967791af5
Diana <dianafbernardo@gmail.com>
2018-11-07 17:47:45 +0100

    exercises added

---
+++ Week-03/Day-03/src/Purple3D.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Purple3D {
+
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+
+        graphics.setColor(new Color(138, 43, 226));
+        int size = 10;
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < 6; i++) {
+            graphics.fill3DRect(x, y, size, size,true );
+            size += 10 * i;
+            x += size;
+            y += size;
+
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
