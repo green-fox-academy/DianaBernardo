7c48a3a4735e84d02eea7f0b106f7810b897883c
Diana <dianafbernardo@gmail.com>
2018-11-13 13:36:35 +0100

    Exercises until Counter

--- Week-04/Day-02/src/Counter/Counter.java
+++ Week-04/Day-02/src/Counter/Counter.java
@@ -1,5 +1,33 @@
 package Counter;
 
 public class Counter {
-    
+    int counter;
+    int initialCounter;
+
+    public Counter(int counter){
+        this.counter = counter;
+        initialCounter = counter;
+    }
+
+    public Counter(){
+        counter = 0;
+        initialCounter = 0;
+    }
+
+
+    public void add(int number) {
+        counter = counter + number;
+    }
+
+    public void add() {
+        counter++;
+    }
+
+    public int get() {
+        return counter;
+    }
+
+    public void reset() {
+        counter = initialCounter;
+    }
 }
