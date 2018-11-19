04e56d49f4824f781710cc9a6efeb70aaf254288
Diana <dianafbernardo@gmail.com>
2018-11-19 20:29:07 +0100

    Added homework

---
+++ Week-05/Day-02/src/Homework/Student.java
@@ -0,0 +1,38 @@
+package Homework;
+
+public class Student {
+    String name;
+    int age;
+    String gender;
+    String previousOrganization;
+    int skippedDays = 0;
+
+    public Student (String name, int age, String gender, String previousOrganization) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.previousOrganization = previousOrganization;
+    }
+
+    public Student() {
+        name = "Jane Doe";
+        age = 30;
+        gender = "Female";
+        previousOrganization = "The School of Life";
+        skippedDays = 0;
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a " + age + "year old " + gender + "from " + previousOrganization + " who skipped " + skippedDays + " days from the course already");
+    }
+
+    public void getGoal() {
+        System.out.println("Be a junior software developer.");
+    }
+
+    public void skipDays(int numberOfDays) {
+        skippedDays++;
+    }
+
+
+}
