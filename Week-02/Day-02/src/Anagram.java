7575d19b8412a9cca130943f627b49ee1a9fab03
Diana <dianafbernardo@gmail.com>
2018-11-23 14:30:22 +0100

    Added Unique

--- Week-02/Day-02/src/Anagram.java
+++ Week-02/Day-02/src/Anagram.java
@@ -9,37 +9,40 @@ import java.util.Scanner;
 
 public class Anagram {
     public static void main(String[] args) {
+      isAnagram("anagram", "margana");
 
-        Scanner scanner = new Scanner(System.in);
-        String input1 = scanner.next();
-
-        Scanner scanner2 = new Scanner(System.in);
-        String input2 = scanner.next();
     }
 
-        public static boolean isAnagram (String input1, String input2){
+        public static boolean isAnagram (String input1, String input2) {
 
-            String s1 = input1.replaceAll("\\s", "");
-            String s2 = input1.replaceAll("\\s", "");
+          boolean status = true;
 
-            boolean status = true;
+          Scanner scanner = new Scanner(System.in);
+          input1 = scanner.next();
 
-            if (s1.length() != s2.length()) {
-                status = false;
-            } else {
-                char[] s1Array = s1.toLowerCase().toCharArray();
-                char[] s2Array = s2.toLowerCase().toCharArray();
+          Scanner scanner2 = new Scanner(System.in);
+          input2 = scanner2.next();
 
-                Arrays.sort(s1Array);
-                Arrays.sort(s2Array);
+          String s1 = input1.replaceAll("\\s", "");
+          String s2 = input2.replaceAll("\\s", "");
+
+
+          if (s1.length() != s2.length()) {
+            status = false;
+          } else {
+            char[] s1Array = s1.toLowerCase().toCharArray();
+            char[] s2Array = s2.toLowerCase().toCharArray();
+
+            Arrays.sort(s1Array);
+            Arrays.sort(s2Array);
 
                 if (s1Array == s2Array) {
-                    status = true;
-                } else {
-                    status = false;
+                  status = true;
+                }
+                  else {
+                  status = false;
                 }
             }
-
             return status;
         }
     }
--- Week-02/Day-02/src/Unique.java
+++ Week-02/Day-02/src/Unique.java
@@ -6,8 +6,26 @@
 //  should print: `[1, 11, 34, 52, 61]`
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Unique {
     public static void main(String[] args){
 
+      List<Integer> original = new ArrayList<>(Arrays.asList(1, 11, 34, 11, 52, 61, 1, 34 ));
+      
+      System.out.println(unique(original));
+
     }
+
+    public static List<Integer> unique (List<Integer> originalList) {
+      List<Integer> unique = new ArrayList<>();
+      for (int i = 0; i < originalList.size(); i++) {
+        if(!unique.contains(originalList.get(i))) {
+          unique.add(originalList.get(i));
+        }
+      }
+      return unique;
+  }
 }
