ea720f56ad0e04794a9b5b45449ee1a4799a03d6
Diana <dianafbernardo@gmail.com>
2018-11-24 13:26:44 +0100

    Added Part of Integer exercise

--- Week-03/Day-02/src/FindPartOfAnInteger.java
+++ Week-03/Day-02/src/FindPartOfAnInteger.java
@@ -1,45 +1,37 @@
 import com.sun.corba.se.spi.ior.ObjectKey;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindPartOfAnInteger {
-    public static void main(String[] args) {
-        //  Create a function that takes a number and a list of numbers as a parameter
-        //  Returns the indeces of the numbers in the list where the first number is part of
-        //  Returns an empty list if the number is not part any of the numbers in the list
-
-        //  Example:
-        // System.out.println(subInt(1, new int[] {1, 11, 34, 52, 61}));
-        //  should print: `[0, 1, 4]`
-        // System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
-        //  should print: '[]'
-
-        int number = 1;
-        List<Integer> integerList = new ArrayList<Integer>();
-        integerList.add(1);
-        integerList.add(11);
-        integerList.add(34);
-        integerList.add(52);
-        integerList.add(61);
+  public static void main(String[] args) {
+    //  Create a function that takes a number and a list of numbers as a parameter
+    //  Returns the indeces of the numbers in the list where the first number is part of
+    //  Returns an empty list if the number is not part any of the numbers in the list
 
 
-        partInteger(number, integerList);
+    System.out.println(partInteger(1, new int[]{1, 11, 34, 52, 61}));
+
+    System.out.println(partInteger(9, new int[]{1, 11, 34, 52, 61}));
+  }
+
+  public static List partInteger(int a, int[] integerList) {
+
+    ArrayList<String> indeces = new ArrayList<>();
+
+    for (int number : integerList) {
+      indeces.add("" + number);
     }
+    String digit = Integer.toString(a);
 
-    public static List partInteger(int number, List<Integer> integerList) {
-
-        ArrayList<Integer> indeces = new ArrayList<Integer>();
-
-        for (int i = 0; i < integerList.size(); i++) {
-            int a = integerList.get(i)%10;
-            int b = integerList.get(i)/10;
-
-            if (a == number || b == number) {
-                indeces.add(integerList.indexOf(i));}
-                }
-
-
-        return
+    ArrayList<Integer> result = new ArrayList<>();
+    for (int i = 0; i < indeces.size(); i++) {
+      if (indeces.get(i).contains(digit)) {
+        result.add(i);
+      }
     }
+    return result;
+  }
 }
+
